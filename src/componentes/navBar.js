c40a5014cb220fcb2e557f7eee97427cac136044
Emilio Ferraro <emilio_1986win@hotmail.com>
2022-02-17 02:44:52 -0300

    desafio1

---
+++ src/componentes/navBar.js
@@ -0,0 +1,22 @@
+
+import './navBar.css';
+
+function Nav() {
+
+    return (
+
+        <>
+            <header className="header">
+
+                <ul className="ul">
+                    <li className="li"><a className="a" href="https://www.google.com" target="_blank"> Home</a></li>
+                    <li><a className="a" href="https://www.google.com" target="_blank"> Productos</a></li>
+                    <li><a className="a" href="https://www.google.com" target="_blank" >Acerca de NINA</a></li>
+                    <li><a className="a" href="https://www.google.com" target="_blank"> Contacto</a></li>
+                </ul>
+
+            </header>
+        </>
+    );
+}
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './componentes/navBar.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Nav from './componentes/navBar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Nav />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
