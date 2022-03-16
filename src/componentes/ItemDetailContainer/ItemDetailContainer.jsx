a3e215d3ff4b6900852129db517283b41f20af4d
Emilio Ferraro <emilio_1986win@hotmail.com>
2022-03-15 22:56:06 -0300

    updating-master

---
+++ src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './ItemDetailContainer.css';
+import { stock } from '../../data/stock';
+import mostrarStock from '../helpers/mostrarStock';
+import ItemDetail from './ItemDetail'
+
+
+function ItemDetailContainer() {
+
+
+    const [item, setItem] = useState([]) //Se almacenan los productos   
+    const [loading, setLoading] = useState(false) //le paso valor inicial d estado  en false
+
+    useEffect(() => {
+
+        setLoading(true)
+        mostrarStock(stock)
+            .then((res) => {
+                //capturo la respuesta y la seteamos en items
+                setItem(res.find((item) => item.id === 1))
+            })
+            .catch((err) => console.log(err))
+            .finally(() => {
+                setLoading(false)   //cambiamos el estado de loading
+            })
+    }, [])
+
+
+    return (
+        <div className='itemLDetailContainer'>
+            {
+                loading ?
+                    <div className='carga'> Cargando.. Por Favor Espere </div>
+                    :
+                    <ItemDetail {...item } />        //Destructury
+            }
+        </div>
+    )
+}
+export default ItemDetailContainer;
